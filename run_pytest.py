4248b1f60c7614d23f6c40a117277357e4e47bb6
yinli <li.yin.MS@faw-vw.com>
2020-10-30 16:50:27 +0800

    新增pytest执行入口

--- run_pytest.py
+++ run_pytest.py
@@ -1,12 +1,18 @@
-import os
+import os,sys
 import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='params of pytest marks self-defined')
     parser.add_argument('--mark',nargs='+',help='marks to run pytest')
     args = parser.parse_args()
-    if args.mark:
+    if len(args.mark) > 1:
         command = 'pytest -qvs ./test -m "{}"'.format(' or '.join(args.mark))
-    else:
+    elif len(args.mark) == 1:
+        command = 'pytest -qvs ./test -m "{}"'.format(args.mark[0])
+    elif not args.mark:
         command = 'pytest -qvs ./test'
+    else:
+        print('参数错误')
+        sys.exit(-1)
+
     os.system(command)
