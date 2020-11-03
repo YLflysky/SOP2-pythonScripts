558971834d4a3df1bcf3c54f45c6fc7796d41cfd
yinli <li.yin.MS@faw-vw.com>
2020-11-03 16:52:40 +0800

    修改测试环境信息

--- run_pytest.py
+++ run_pytest.py
@@ -6,14 +6,7 @@ import pytest
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='params of pytest marks self-defined')
     parser.add_argument('--mark',nargs='+',help='marks to run pytest')
-    parser.add_argument('--env',type=str,help='choose env to run test case',choices=['DEV','SIT','UAT'])
     args = parser.parse_args()
-    if not args.env:
-        print('请指定测试环境:{}'.format('DEV,SIT,UAT'))
-        sys.exit(-1)
-    else:
-        os.environ['ENV'] = args.env
-        os.environ['GATE'] = 'false'
     print('marks:{}'.format(args.mark))
     if not args.mark:
         pytest.main(
--- test/test_demo.py
+++ test/test_demo.py
@@ -5,5 +5,9 @@ def test_01():
     assert 1 ==1
 
 @pytest.mark.sergio
-def test_01():
+def test_02():
     assert 'sergio'
+
+@pytest.mark.lyin
+def test_03():
+    assert 1 == 2
