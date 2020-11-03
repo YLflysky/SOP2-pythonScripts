203e2c38aa5ec70127c82de84d6e58240e2eae99
yinli <li.yin.MS@faw-vw.com>
2020-11-03 17:00:16 +0800

    修改run_pytest

--- run_pytest.py
+++ run_pytest.py
@@ -5,19 +5,21 @@ import pytest
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='params of pytest marks self-defined')
-    parser.add_argument('--mark',nargs='+',help='marks to run pytest')
+    parser.add_argument('--mark',help='marks to run pytest')
     args = parser.parse_args()
+
     print('marks:{}'.format(args.mark))
     if not args.mark:
         pytest.main(
             ['-q', '-m' './test', '--alluredir=/data/allure-results'])
     else:
-        if len(args.mark) > 1:
+        marks = args.mark.split(',')
+        if len(marks) > 1:
             pytest.main(
-                ['-q', '-m' './test', '--alluredir=/data/allure-results', '-m', "{}".format(' or '.join(args.mark))])
-        elif len(args.mark) == 1:
+                ['-q', '-m' './test', '--alluredir=/data/allure-results', '-m', "{}".format(' or '.join(marks))])
+        elif len(marks) == 1:
             pytest.main(
-                ['-q', '-m' './test', '--alluredir=/data/allure-results', '-m', "{}".format(args.mark[0])])
+                ['-q', '-m' './test', '--alluredir=/data/allure-results', '-m', "{}".format(marks[0])])
         else:
             print('参数错误')
             sys.exit(-1)
