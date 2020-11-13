fc79085d7a289c3b0c25644184807f0253abc79a
yinli <li.yin.MS@faw-vw.com>
2020-11-13 16:18:23 +0800

    修改用例

--- test/conftest.py
+++ test/conftest.py
@@ -7,7 +7,7 @@ import os
 if not os.getenv('GATE'):
     os.environ['GATE'] = 'false'
 if not os.getenv('ENV'):
-    os.environ['ENV'] = 'DEV'
+    os.environ['ENV'] = 'SIT'
 
 o = Base()
 bm = BMOrder()
