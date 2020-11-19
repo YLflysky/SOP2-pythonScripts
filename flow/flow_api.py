6df6947fe899fb3d4c802717485f747344da16c0
yinli <li.yin.MS@faw-vw.com>
2020-11-19 13:45:46 +0800

    修改用例

--- flow/flow_api.py
+++ flow/flow_api.py
@@ -8,6 +8,7 @@ class Flow(Base):
 
         self.hu_url = self.read_conf('sop2_env.conf', self.env, 'hu_host')
         self.flow_url = self.read_conf('sop2_env.conf', self.env, 'flow_host')
+        self.second_url = self.read_conf('sop2_env.conf', self.env, '2nd_host')
 
     def bm_get_flow_detail(self, goods_id):
         '''
@@ -48,7 +49,7 @@ class Flow(Base):
         return b
 
     def common_callback(self, id, category, status, origin_id, enterprise_id='2100010000',**kwargs):
-        url = self.flow_url + '/cmcc/notify/CI_CommonNotification'
+        url = self.flow_url + '/cmcc/notify/commonNtify'
         data = {'enterpriseId': enterprise_id,
                 'multiRecords': [{'id': id, 'idCategory': category, 'status': status, 'originalRequestId': origin_id,**kwargs}]}
 
@@ -114,13 +115,21 @@ class Flow(Base):
         assert c == 200
         return b
 
+    def flow_notify(self,id,date,rule,asset_type,asset_id,package_id,vin):
+        url = self.flow_url + '/cmcc/notify/custSimNotification'
+        data ={'requestId':id,'requestDateTime':date,'notificationFlagRule':rule,'assetType':asset_type,
+               'assetId':asset_id,'packageId':package_id,'vin':vin}
+        c,b = self.do_post(url,data)
+        self.assert_msg(c,b)
+
+
 
 if __name__ == '__main__':
     import os
     import random
 
     os.environ['GATE'] = 'false'
-    os.environ['ENV'] = 'UAT'
+    os.environ['ENV'] = 'DEV'
     flow = Flow()
     # flow.get_qr_code('e00c66a3ad7a4964911cbaf475bcca9b','111','20201113094813034827392','ALI_PAY',flow.f.md5())
     success_attr={'thirdPartyPaymentSerial':'qq995939534','payChannel':'ALI_PAY','paidTime':flow.time_delta(formatted='%Y%m%d%H%M%S')}
@@ -128,7 +137,9 @@ if __name__ == '__main__':
     #                      additionalAttrs=success_attr)
     # flow.flow_detail(100)
     # flow.goods_list(['WIFI_FLOW'])
-    flow.bm_get_flow_detail('100')
+    # flow.bm_get_flow_detail('100')
     # flow.bm_create_flow_order(goods_id='5b7cf4f565914cab86cf71ef9ca34e99',aid='qq995939534',vin='LFVSOP2TEST000353',quantity=1)
     # flow.bm_goods_list('995939534','WIFI_FLOW')
     # flow.sign_result_callback(aid=flow.f.pyint(),channel=1,notify_type=2,status=1)
+    flow.flow_notify(id='1',date=flow.time_delta(formatted='%Y%m%d%H%M%S'),rule=0.1,
+                     asset_type='iccid',asset_id='995939534',package_id='P1001123577',vin='LFV2A11KXA3030241')
